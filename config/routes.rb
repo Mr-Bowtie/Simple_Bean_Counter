ffa4ea7c3411c55093894fc1f2b6532625abd896
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-06-06 07:32:17 -0400

    finish gem setup, generate rbi's for sorbet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# typed: false
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'info_sheet#show'
 end
