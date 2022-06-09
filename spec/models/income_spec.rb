41b967b6bd3e97d8df10f5a8508d1774efe00c77
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-06-09 07:37:26 -0400

    generated specs

--- spec/models/income_spec.rb
+++ spec/models/income_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: incomes
+#
+#  id         :bigint           not null, primary key
+#  amount     :decimal(5, 2)
+#  period     :integer          default("monthly")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Income, type: :model do
