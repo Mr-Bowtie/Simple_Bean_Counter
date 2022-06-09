832e925a52daace7622df85229622046396499f4
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-06-09 07:36:14 -0400

    adding inital routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'info_sheet#show'
+  resources :info_sheets
+  root "info_sheets#index"
 end
